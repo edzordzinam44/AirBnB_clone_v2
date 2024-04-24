e19850f159c824154a36e21f4e13398889f5e567
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-04-24 21:30:21 +0000

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,45 +3,54 @@
 start Flask application
 """
 
+
 from flask import Flask, render_template
+
+
+# Create a Flask application instance
 app = Flask(__name__)
 
 
+# Define a route for the homepage
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def hello():
     return 'Hello HBNB!'
 
 
+# Define a route for /hbnb
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
     return 'HBNB'
 
 
+# Define a route for /c/<text>
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def c_text(text):
+    # Replace underscore (_) symbols with a space
+    text = text.replace('_', ' ')
+    return 'C ' + text
 
 
-@app.route('/python', strict_slashes=False)
+# Define a route for /python/<text>
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+def python_text(text):
+    # Replace underscore (_) symbols with a space
+    text = text.replace('_', ' ')
+    return 'Python ' + text
 
 
+# Define a route for /number/<n>
 @app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
-    """display “n is a number” only if n is an integer"""
-    return "{:d} is a number".format(n)
+def number(n):
+    return f'{n} is a number'
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numbersandtemplates(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
